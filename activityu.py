abf7d6b4d596e3280d5da5330e11ce559c0f4cf4
omon212 <omonnuloraimkulov@gmail.com>
2024-07-01 22:52:26 +0500

    commit

--- activityu.py
+++ activityu.py
@@ -1,7 +1,7 @@
 import os
 from random import randint
 
-for i in range(1, 550):
+for i in range(1, 650):
     for j in range(0, randint(1, 10)):
         d = str(i) + ' days ago'
         with open('file.txt', 'a') as file:
