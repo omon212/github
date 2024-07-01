6ee3534be91ffbaba68cb0416b5e231430593ca6
omon212 <omonnuloraimkulov@gmail.com>
2024-07-01 23:25:59 +0500

    commit

---
+++ Keyboards/inline.py
@@ -0,0 +1,9 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+github = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text="Your Github Profile", url="https://github.com/omon212"),
+        ]
+    ]
+)
--- bot.py
+++ bot.py
@@ -1,16 +1,19 @@
 import logging
 from aiogram import Dispatcher, Bot, executor, types
+from Keyboards.inline import github
 
-TOKEN = ""
+TOKEN = "7108274430:AAFYuNKtM2JiIlyfHbzd6L717tAONULNx7A"
 
-bot = Bot(token=TOKEN)
+bot = Bot(token=TOKEN, parse_mode="HTML")
 dp = Dispatcher(bot)
 logging.basicConfig(level=logging.INFO)
 
 
 @dp.message_handler(commands=['start'])
 async def welcome(message: types.Message):
+    await message.answer("Hello <b>Omonullo Raimkulov</b>")
 
+    await message.answer("Pushed to your github profile today", reply_markup=github)
 
 
 if __name__ == '__main__':
