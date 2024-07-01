7632fa32e0e8ce109dca96ca44e0385f6b724c31
omon212 <omonnuloraimkulov@gmail.com>
2024-07-01 22:25:21 +0500

    commit

--- activityu.py
+++ activityu.py
@@ -5,8 +5,8 @@ for i in range(1, 1300):
     for j in range(0, randint(1, 10)):
         d = str(i) + ' days ago'
         with open('file.txt', 'a') as file:
-            file.write(d)
+            file.write(d + '\n')
         os.system('git add .')
-        os.system('git commit --date="' + d + '"-m "commit"')
+        os.system('git commit --date="' + d + '" -m "commit"')
 
 os.system('git push -u origin main')
