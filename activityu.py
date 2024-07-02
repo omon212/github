6c15afe2daefe5584e8763416783f935806eca40
omon212 <omonnuloraimkulov@gmail.com>
2024-07-02 22:25:06 +0500

    Initial commit

--- activityu.py
+++ activityu.py
@@ -6,7 +6,7 @@ for i in range(1, 1300):
         d = str(i) + ' days ago'
         with open('file.txt', 'a') as file:
             file.write(d)
-        os.system('git add.')
+        os.system('git add .')
         os.system('git commit --date="' + d + '"-m "commit"')
 
 os.system('git push -u origin main')
