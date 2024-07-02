9254d560a81811557d410981fcbc5c36549402e3
omon212 <omonnuloraimkulov@gmail.com>
2024-07-02 23:45:04 +0500

    hello

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
-import logging
+import logging, os, datetime, time, pytz
+from random import randint
 from aiogram import Dispatcher, Bot, executor, types
 from Keyboards.inline import github
 
@@ -8,12 +9,29 @@ bot = Bot(token=TOKEN, parse_mode="HTML")
 dp = Dispatcher(bot)
 logging.basicConfig(level=logging.INFO)
 
+uz_time = pytz.timezone('Asia/Tashkent')
+user = 6457971132
+
 
 @dp.message_handler(commands=['start'])
 async def welcome(message: types.Message):
     await message.answer("Hello <b>Omonullo Raimkulov</b>")
-
-    await message.answer("Pushed to your github profile today", reply_markup=github)
+    while True:
+        now = datetime.datetime.now(uz_time)
+        if now.hour == 23 and now.minute == 45:
+            for i in range(1):
+                for j in range(0, randint(1, 10)):
+                    d = str(i) + ' days ago'
+                    with open('file.txt', 'a') as file:
+                        file.write(d + '\n')
+                    os.system('git add .')
+                    os.system('git commit --date="' + d + '" -m "hello"')
+            os.system('git push -u origin main')
+            await bot.send_message(user, "Hello <b>Omonullo Raimkulov</b>")
+            await bot.send_message(user, "Pushed to your github profile today", reply_markup=github)
+            time.sleep(60)
+        else:
+            time.sleep(30)
 
 
 if __name__ == '__main__':
--- test.py
+++ test.py
@@ -1,12 +1,30 @@
-import os
-from random import randint
+# import os
+# from random import randint
+#
+# for i in range(1):
+#     for j in range(0, randint(1, 10)):
+#         d = str(i) + ' days ago'
+#         with open('file.txt', 'a') as file:
+#             file.write(d + '\n')
+#         os.system('git add .')
+#         os.system('git commit --date="' + d + '" -m "hello"')
+#
+# os.system('git push -u origin main')
 
-for i in range(1):
-    for j in range(0, randint(1, 10)):
-        d = str(i) + ' days ago'
-        with open('file.txt', 'a') as file:
-            file.write(d + '\n')
-        os.system('git add .')
-        os.system('git commit --date="' + d + '" -m "hello"')
 
-os.system('git push -u origin main')
+import datetime
+import time
+import pytz
+
+# Define the Uzbekistan time zone
+uzbekistan_tz = pytz.timezone('Asia/Tashkent')
+
+while True:
+    now = datetime.datetime.now(uzbekistan_tz)
+    if now.hour == 23 and now.minute == 41:
+        print_hello_world()
+        time.sleep(60)
+    else:
+        time.sleep(30)
+
+
